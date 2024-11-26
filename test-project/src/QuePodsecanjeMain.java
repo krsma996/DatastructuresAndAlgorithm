1368d6112534cbab75a264f19d09ca056488268a
NikolaKrsmanovic1996 <nikolakrsmanovic223@gmail.com>
2024-11-27 00:26:07 +0100

    que i stack objasnjeni do kraja
    objasnjeni do kraja que i stack

--- test-project/src/QuePodsecanjeMain.java
+++ test-project/src/QuePodsecanjeMain.java
@@ -2,7 +2,16 @@
 public class QuePodsecanjeMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		QuePodsecanje que = new QuePodsecanje(1);
+		que.enque(2);
+		que.enque(3);
+		que.enque(4);
+		que.enque(5);
+		que.deque();
+		que.deque();
+		que.getFirst();
+		que.getLast();
+		que.printList();
 
 	}
 
