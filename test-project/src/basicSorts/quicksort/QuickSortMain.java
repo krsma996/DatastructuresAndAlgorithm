f7744a59c351e0b2b663b5c64fa5a06b4130e360
NikolaKrsmanovic1996 <nikolakrsmanovic223@gmail.com>
2025-02-01 19:21:34 +0100

    quickSort finishe
    objasnjen quickSort usrani do kraja

--- test-project/src/basicSorts/quicksort/QuickSortMain.java
+++ test-project/src/basicSorts/quicksort/QuickSortMain.java
@@ -5,22 +5,37 @@ import java.util.Arrays;
 public class QuickSortMain {
 
 	public static void main(String[] args) {
-		int array1[] = {4,2,3,6,5,8,7};
-		int returnedIndex = pivot(array1, 0);
-		System.out.println("Returned Index = " + returnedIndex);
+		int array1[] = {1,4,2,3,6,5,8,7};
+
 		System.out.println(Arrays.toString(array1));
-		System.out.println("-----------------------");
+		System.out.println("-----------------------");		
+		quickSort(array1);
+		System.out.println(Arrays.toString(array1));
 	}
 
+	
+	
+	public static void quickSortHelper(int array[],int left,int right) {
+		if(left < right) {
+			int pivotIndex = pivot(array, left, right);
+			quickSortHelper(array, left, pivotIndex - 1);
+			quickSortHelper(array, pivotIndex +1, right);	
+		}		
+	}
+	
+	public static void quickSort(int array[]) {
+		quickSortHelper(array, 0, array.length-1);
+	}
+	
 	
 	//fora je ovde sto se uzme prvi element niza 
 	//i na osnou te vrednosti niza radi se swapovanje
 	// swapIndex i pivot index su na 0 tj pocinju na indexu 0;
 	// a kada se vrti petlja gurne se pokazivac desno za 1
 	// i onda se radi porededjenje
-	private static int pivot(int array1[],int pivotIndex) {
+	private static int pivot(int array1[],int pivotIndex,int endIndex) {
 		int swapIndex = pivotIndex;
-		for(int i = pivotIndex+1;i <= array1.length -1;i++) {
+		for(int i = pivotIndex+1;i <= endIndex;i++) {
 			//da li je 2 vrednost u nizu manja od prve vrednosti u nizu 
 			//ako jeste radimo swap
 			if(array1[i]<array1[pivotIndex]) {
