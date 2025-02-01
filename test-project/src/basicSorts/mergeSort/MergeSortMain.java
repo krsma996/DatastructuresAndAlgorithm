5f47b6e6042bf47623571a7ab83b6267e620bc1d
NikolaKrsmanovic1996 <nikolakrsmanovic223@gmail.com>
2025-02-01 17:04:36 +0100

    merge sort reffactor
    refaktorisanje merge sorta objasnjenje

--- test-project/src/basicSorts/mergeSort/MergeSortMain.java
+++ test-project/src/basicSorts/mergeSort/MergeSortMain.java
@@ -24,13 +24,15 @@ public class MergeSortMain {
 	}
 
 	public static int[] merge(int array1[], int array2[]) {
-		int commbined[] = new int[array1.length + array2.length];
+		int commbined[] = new int[array1.length + array2.length]; // rezultat na kraju koji vraca sortiranu listu
 		int index = 0; // ovo ce biti index od combied[] array
 		int i = 0; // ovo se koristi da se iterira 1 polovina
 		int j = 0; // a ovo se koristi da se iterira 2 polovina
 		//dakle fora je ovde sto ce da u toj listi COMBINED da ocekaje 1 po 1 element da bude sortiran
 		//i moze se desi da array1 vise nema elemenete a array2 ima
 		// i onda se posoebno za svaki while petlja opaljuje da vidi dal ima nesto u njima
+		
+		// ovo se vrti kada se jedno od ove 2 liste nebude prazna
 		while (i < array1.length && j < array2.length) {
 			// i < array1.length da li je prvi array1 prazan
 			// j <i < array2.length da li je drugi array2 prazan
@@ -44,12 +46,15 @@ public class MergeSortMain {
 				j++;
 			}
 		}
+		// a kada bude prazna onda proletimo posebno sa leve
 		// i onda ako ima nesto sto je ostalo u array1.lenght ti ih dodaj u combined
 		while (i < array1.length) {
 			commbined[index] = array1[i];
 			index++;
 			i++;
 		}
+		
+		// i onda prevrtimo posebno sa desne
 		//a ako ima nesto u 2 array ti ih dodaj isto u combined
 		// u smisli da su ostalo jos neki elementi da se ubace 
 		while (j < array2.length) {
@@ -64,14 +69,16 @@ public class MergeSortMain {
 	//ovo cepka listu na pola 
 	// i onda od njih radi merge swapuje ih
 	public static int[] mergeSort(int [] array) {
-		 if (array.length == 1) return array; //vratice rezultat zadnjej vrednosti u call stacku pa ce krene da pop
-	        int midIndex = array.length/2;
-	        //ovde kazes copy mi array kreni od 0 do MIN index-a a to je polovina njegova zadnja
+		 if (array.length == 1) return array; //kada se poziva rekuzivno  i kada duzina bude bila 1 e onda ce poceti da POP iz call stacka sve vrednosti
+	        
+		 // uzece niz koji mu je prosledjen i podelice duzinu na pola
+		 	int midIndex = array.length/2;
+		 	// ovde od te polovine ce da kopira vrednosti u niz left i rekuznivno ce se pozivati
 	        int[] left = mergeSort(Arrays.copyOfRange(array, 0, midIndex));
-	        //a ovde od polove zadnje do duzine
+	        // kad zavrsi levu stranu onda ide i cepka desnu stranu i kopira vrednosti  i isto ce se rekuzivno pozivati
 	        int[] right = mergeSort(Arrays.copyOfRange(array, midIndex, array.length));
-
-	        //kad u callsteck rekuzivnom pozivu ima 1 vrednost i right isto ima 2 vrednost tad radi merge ovde ispod
+	        
+	        // a ovde ce da radi swapovanje
 	        return merge(left, right);
 	}
 }
