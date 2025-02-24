585c6cf35050144987ca0a19b4fc194a95d41407
NikolaKrsmanovic1996 <nikolakrsmanovic223@gmail.com>
2025-02-24 20:19:16 +0100

    removeElement in []
    removeElement in [] leetcode exercise

---
+++ test-project/src/leetcodeExercise/RemoveElementMain.java
@@ -0,0 +1,21 @@
+package leetcodeExercise;
+
+public class RemoveElementMain {
+
+	public static void main(String[] args) {
+		int niz1[] = {1,3,4,5,6};
+		removeElement(niz1,4);
+	}
+
+	public static int removeElement(int[] nums, int val) {
+			int nextIndex = 0;
+	        for(int i =0;i<=nums.length - 1;i++){
+	            if(nums[i] != val){
+	            	nums[nextIndex] = nums[i];
+	            	nextIndex++;
+	            }
+	        }
+	        return nextIndex;
+	}
+
+}
