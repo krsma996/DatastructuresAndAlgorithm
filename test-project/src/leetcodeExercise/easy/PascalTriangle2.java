575d4229a1dad548404a61138c04e516b5b977a9
NikolaKrsmanovic1996 <nikolakrsmanovic223@gmail.com>
2025-03-03 18:13:39 +0100

    pascalTriangle2
    pascal triangle2 resenje

---
+++ test-project/src/leetcodeExercise/easy/PascalTriangle2.java
@@ -0,0 +1,36 @@
+package leetcodeExercise.easy;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class PascalTriangle2 {
+
+	public static void main(String[] args) {
+		int rowsNum = 3;
+		List<Integer> generatedNums = generateRow(rowsNum);
+		System.out.println(Arrays.asList(generatedNums));
+	}
+
+	public static List<Integer> generateRow(int rowIndex) {
+		List<Integer> result = new ArrayList<>();
+		if (rowIndex < 0)return result; // Edge case
+		
+		result.add(1); // Prvi element uvek 1
+		for (int i = 1; i <= rowIndex; i++) {
+			// Kreiraj novu listu koja će sadržati sledeći red
+			List<Integer> newRow = new ArrayList<>();
+			newRow.add(1); // Početak reda je uvek 1
+
+			// Popunjavaj red sa zbirima prethodnih elemenata
+			for (int j = 1; j < result.size(); j++) {
+				newRow.add(result.get(j) + result.get(j - 1));
+			}
+
+			newRow.add(1); // Kraj reda je uvek 1
+			//ta result ce biti new row
+			result = newRow; // Zameni prethodni red sa novim
+		}
+		return result;
+	}
+}
